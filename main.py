7699fcbd47e7fd86df6a6f17f84711c6f08d0419
jigiciak <marcin.lukasik2@gmail.com>
2020-09-11 14:02:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,38 +13,55 @@ class PowerMixer(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.timer = QTimer()
         self.ui.pushMIDI.clicked.connect(self.control_timer)
+
         self.ui.listProcess0.currentTextChanged.connect(lambda x: self.assign_slider(self.ui.listProcess0.currentText(),
-                                                                                     self.ui.sliderProcess0))
+                                                                                     self.ui.sliderProcess0,
+                                                                                     self.ui.processValue0))
         self.ui.listProcess1.currentTextChanged.connect(lambda x: self.assign_slider(self.ui.listProcess1.currentText(),
-                                                                                     self.ui.sliderProcess1))
+                                                                                     self.ui.sliderProcess1,
+                                                                                     self.ui.processValue1))
         self.ui.listProcess2.currentTextChanged.connect(lambda x: self.assign_slider(self.ui.listProcess2.currentText(),
-                                                                                     self.ui.sliderProcess2))
+                                                                                     self.ui.sliderProcess2,
+                                                                                     self.ui.processValue2))
         self.ui.listProcess3.currentTextChanged.connect(lambda x: self.assign_slider(self.ui.listProcess3.currentText(),
-                                                                                     self.ui.sliderProcess3))
+                                                                                     self.ui.sliderProcess3,
+                                                                                     self.ui.processValue3))
         self.ui.listProcess4.currentTextChanged.connect(lambda x: self.assign_slider(self.ui.listProcess4.currentText(),
-                                                                                     self.ui.sliderProcess4))
+                                                                                     self.ui.sliderProcess4,
+                                                                                     self.ui.processValue4))
         self.ui.listProcess5.currentTextChanged.connect(lambda x: self.assign_slider(self.ui.listProcess5.currentText(),
-                                                                                     self.ui.sliderProcess5))
+                                                                                     self.ui.sliderProcess5,
+                                                                                     self.ui.processValue5))
         self.ui.listProcess6.currentTextChanged.connect(lambda x: self.assign_slider(self.ui.listProcess6.currentText(),
-                                                                                     self.ui.sliderProcess6))
+                                                                                     self.ui.sliderProcess6,
+                                                                                     self.ui.processValue6))
         self.ui.listProcess7.currentTextChanged.connect(lambda x: self.assign_slider(self.ui.listProcess7.currentText(),
-                                                                                     self.ui.sliderProcess7))
+                                                                                     self.ui.sliderProcess7,
+                                                                                     self.ui.processValue7))
         self.ui.sliderProcess0.valueChanged.connect(lambda x: self.update_volume(self.ui.sliderProcess0,
-                                                                                 self.processList.index(self.ui.listProcess0.currentText())))
+                                                                                 self.processList.index(self.ui.listProcess0.currentText()),
+                                                                                 self.ui.processValue0))
         self.ui.sliderProcess1.valueChanged.connect(lambda x: self.update_volume(self.ui.sliderProcess1,
-                                                                                 self.processList.index(self.ui.listProcess1.currentText())))
+                                                                                 self.processList.index(self.ui.listProcess1.currentText()),
+                                                                                 self.ui.processValue1))
         self.ui.sliderProcess2.valueChanged.connect(lambda x: self.update_volume(self.ui.sliderProcess2,
-                                                                                 self.processList.index(self.ui.listProcess2.currentText())))
+                                                                                 self.processList.index(self.ui.listProcess2.currentText()),
+                                                                                 self.ui.processValue2))
         self.ui.sliderProcess3.valueChanged.connect(lambda x: self.update_volume(self.ui.sliderProcess3,
-                                                                                 self.processList.index(self.ui.listProcess3.currentText())))
+                                                                                 self.processList.index(self.ui.listProcess3.currentText()),
+                                                                                 self.ui.processValue3))
         self.ui.sliderProcess4.valueChanged.connect(lambda x: self.update_volume(self.ui.sliderProcess4,
-                                                                                 self.processList.index(self.ui.listProcess4.currentText())))
+                                                                                 self.processList.index(self.ui.listProcess4.currentText()),
+                                                                                 self.ui.processValue4))
         self.ui.sliderProcess5.valueChanged.connect(lambda x: self.update_volume(self.ui.sliderProcess5,
-                                                                                 self.processList.index(self.ui.listProcess5.currentText())))
+                                                                                 self.processList.index(self.ui.listProcess5.currentText()),
+                                                                                 self.ui.processValue5))
         self.ui.sliderProcess6.valueChanged.connect(lambda x: self.update_volume(self.ui.sliderProcess6,
-                                                                                 self.processList.index(self.ui.listProcess6.currentText())))
+                                                                                 self.processList.index(self.ui.listProcess6.currentText()),
+                                                                                 self.ui.processValue6))
         self.ui.sliderProcess7.valueChanged.connect(lambda x: self.update_volume(self.ui.sliderProcess7,
-                                                                                 self.processList.index(self.ui.listProcess7.currentText())))
+                                                                                 self.processList.index(self.ui.listProcess7.currentText()),
+                                                                                 self.ui.processValue7))
         self.midiIn = rtmidi.MidiIn()
         self.timer.timeout.connect(self.read_midi)
         self.processList = ["None"]
@@ -117,15 +134,19 @@ class PowerMixer(QtWidgets.QMainWindow):
         self.ui.listProcess7.addItems(processes)
 
     @staticmethod
-    def assign_slider(process, slider):
+    def assign_slider(process, slider, value):
         if process != "None":
             slider.setEnabled(1)
+            value.setEnabled(1)
         else:
             slider.setDisabled(1)
+            value.setDisabled(1)
 
-    def update_volume(self, slider, volume):
+    def update_volume(self, slider, volume, value):
         # print(volume, slider.value())
         self.volumeList[volume].SetMasterVolume(slider.value()/100, None)
+        value.setText(str(slider.value()))
+        print(slider.value())
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
